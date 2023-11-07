137fe85e4973a06a5b682cf80dd16393b373832d
Julian Klaiber <julian@klaiber.me>
2023-11-07 16:27:40 +0000

    Update to new config format

--- pkg/maps/client_map_components.go
+++ pkg/maps/client_map_components.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/cilium/ebpf"
+	"github.com/hawkv6/hawkwing/internal/config"
 )
 
 type InnerMap struct {
@@ -69,20 +70,53 @@ func NewLookupMap(spec *ebpf.MapSpec) *LookupMap {
 func (lm *LookupMap) BuildWith(inners map[string]*InnerMap) error {
 	lookupContents := []ebpf.MapKV{}
 	for key, inner := range inners {
-		formattedDnsName, err := FormatDNSName(key)
-		if err != nil {
-			return fmt.Errorf("could not format DNS name: %s", err)
+		if config.Params.Services[key].DomainName != "" {
+			formattedDnsName, err := FormatDNSName(config.Params.Services[key].DomainName)
+			if err != nil {
+				return fmt.Errorf("could not format DNS name: %s", err)
+			}
+			lookupContents = append(lookupContents, ebpf.MapKV{
+				Key:   formattedDnsName,
+				Value: uint32(inner.ID),
+			})
 		}
-		lookupContents = append(lookupContents, ebpf.MapKV{
-			Key:   formattedDnsName,
-			Value: uint32(inner.ID),
-		})
 	}
 
 	lm.Map.spec.Contents = lookupContents
 	err := lm.Map.OpenOrCreate()
 	if err != nil {
 		return fmt.Errorf("could not create lookup map: %s", err)
+	}
+	return nil
+}
+
+type ReverseMap struct {
+	Map
+}
+
+func NewReverseMap(spec *ebpf.MapSpec) *ReverseMap {
+	return &ReverseMap{
+		Map: Map{spec: spec},
+	}
+}
+
+func (rm *ReverseMap) BuildWith(inners map[string]*InnerMap) error {
+	reverseMapContents := []ebpf.MapKV{}
+	for key, inner := range inners {
+		if config.Params.Services[key].DomainName == "" && len(config.Params.Services[key].Ipv6Addresses) > 0 {
+			for _, ipv6Address := range config.Params.Services[key].Ipv6Addresses {
+				reverseMapContents = append(reverseMapContents, ebpf.MapKV{
+					Key:   Ipv6ToInet6(ipv6Address),
+					Value: uint32(inner.ID),
+				})
+			}
+		}
+	}
+
+	rm.Map.spec.Contents = reverseMapContents
+	err := rm.Map.OpenOrCreate()
+	if err != nil {
+		return fmt.Errorf("could not create reverse map: %s", err)
 	}
 	return nil
 }
