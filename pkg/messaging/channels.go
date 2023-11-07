21357ecec89e5d219dd845db3ee02a49ff0bed3b
Julian Klaiber <julian@klaiber.me>
2023-11-07 16:27:58 +0000

    Update to new proto message format

--- pkg/messaging/channels.go
+++ pkg/messaging/channels.go
@@ -1,27 +1,30 @@
 package messaging
 
-import "github.com/hawkv6/hawkwing/pkg/api"
+import (
+	"github.com/hawkv6/hawkwing/pkg/api"
+	"github.com/hawkv6/hawkwing/pkg/entities"
+)
 
 type MessagingChannels struct {
-	ChMessageIntentRequest  chan *api.Intent
-	ChMessageIntentResponse chan *api.Response
+	ChMessageIntentRequest  chan *api.PathRequest
+	ChMessageIntentResponse chan *api.PathResult
 }
 
 func NewMessagingChannels() *MessagingChannels {
 	return &MessagingChannels{
-		ChMessageIntentRequest:  make(chan *api.Intent),
-		ChMessageIntentResponse: make(chan *api.Response),
+		ChMessageIntentRequest:  make(chan *api.PathRequest),
+		ChMessageIntentResponse: make(chan *api.PathResult),
 	}
 }
 
 type AdapterChannels struct {
-	ChAdapterIntentRequest  chan *IntentRequest
-	ChAdapterIntentResponse chan *IntentResponse
+	ChAdapterIntentRequest  chan *entities.PathRequest
+	ChAdapterIntentResponse chan *entities.PathResult
 }
 
 func NewAdapterChannels() *AdapterChannels {
 	return &AdapterChannels{
-		ChAdapterIntentRequest:  make(chan *IntentRequest),
-		ChAdapterIntentResponse: make(chan *IntentResponse),
+		ChAdapterIntentRequest:  make(chan *entities.PathRequest),
+		ChAdapterIntentResponse: make(chan *entities.PathResult),
 	}
 }
--- pkg/messaging/client.go
+++ pkg/messaging/client.go
@@ -15,20 +15,23 @@ var log = logging.DefaultLogger.WithField("subsystem", Subsystem)
 const Subsystem = "go-messaging"
 
 type MessagingClient struct {
-	api.IntentServiceClient
+	api.IntentControllerClient
 	messagingChannels *MessagingChannels
 	conn              *grpc.ClientConn
+	streamErrors      chan error
 }
 
 func NewMessagingClient(messagingChannels *MessagingChannels) *MessagingClient {
 	return &MessagingClient{
 		messagingChannels: messagingChannels,
+		streamErrors:      make(chan error),
 	}
 }
 
 func (c *MessagingClient) Start() {
 	c.connect()
-	c.handleIntentRequest()
+	// c.handleIntentRequest()
+	go c.manageStreams()
 
 }
 
@@ -43,24 +46,79 @@ func (c *MessagingClient) connect() {
 			continue
 		}
 		c.conn = conn
-		c.IntentServiceClient = api.NewIntentServiceClient(conn)
+		c.IntentControllerClient = api.NewIntentControllerClient(conn)
 		break
 	}
 	log.Printf("connected to %s", connectionAddress)
 }
 
-func (c *MessagingClient) handleIntentRequest() {
-	go func() {
-		for {
-			intentRequest := <-c.messagingChannels.ChMessageIntentRequest
-			intentResponse, err := c.GetIntentDetails(context.Background(), intentRequest)
-			if err != nil {
-				log.Printf("failed to get intent details: %v, retrying...", err)
-				c.connect()
-				continue
-			}
-			c.messagingChannels.ChMessageIntentResponse <- intentResponse
-			log.Printf("received intent response for [domain | intent]: [%s | %s]", intentResponse.DomainName, intentEnumToString(intentResponse.Intent))
+func (c *MessagingClient) manageStreams() {
+	for {
+		ctx, cancel := context.WithCancel(context.Background())
+		stream, err := c.IntentControllerClient.GetIntentPath(ctx)
+		if err != nil {
+			log.Printf("failed to get intent path: %v, retrying...", err)
+			c.streamErrors <- err
+			continue
 		}
-	}()
+
+		go c.handleGetIntentPathRequests(ctx, stream)
+		go c.handleGetIntentPathResults(ctx, stream)
+
+		err = <-c.streamErrors
+		log.Printf("error received from stream: %v", err)
+
+		cancel()
+
+		log.Fatalf("stream error: %v", err)
+	}
 }
+
+func (c *MessagingClient) handleGetIntentPathRequests(ctx context.Context, stream api.IntentController_GetIntentPathClient) {
+	for {
+		select {
+		case request := <-c.messagingChannels.ChMessageIntentRequest:
+			if err := stream.Send(request); err != nil {
+				log.Printf("failed to send message: %v", err)
+				c.streamErrors <- err
+				return
+			}
+			log.Printf("sent intent request for %s", request.Ipv6DestinationAddress)
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (c *MessagingClient) handleGetIntentPathResults(ctx context.Context, stream api.IntentController_GetIntentPathClient) {
+	for {
+		in, err := stream.Recv()
+		if err != nil {
+			log.Printf("failed to receive message: %v", err)
+			c.streamErrors <- err
+			return
+		}
+		log.Printf("received intent result for %s", in.Ipv6DestinationAddress)
+		select {
+		case c.messagingChannels.ChMessageIntentResponse <- in:
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+// func (c *MessagingClient) handleIntentRequest() {
+// 	go func() {
+// 		for {
+// 			intentRequest := <-c.messagingChannels.ChMessageIntentRequest
+// 			intentResponse, err := c.GetIntentDetails(context.Background(), intentRequest)
+// 			if err != nil {
+// 				log.Printf("failed to get intent details: %v, retrying...", err)
+// 				c.connect()
+// 				continue
+// 			}
+// 			c.messagingChannels.ChMessageIntentResponse <- intentResponse
+// 			log.Printf("received intent response for [domain | intent]: [%s | %s]", intentResponse.DomainName, intentEnumToString(intentResponse.Intent))
+// 		}
+// 	}()
+// }
